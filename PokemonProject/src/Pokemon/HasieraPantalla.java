6bd4557818f3b6dfa1e4aa48980491530ef56888
Oier Cao <Oier Cao@MSI>
2023-03-18 10:53:59 +0100

    " clases nuevas

---
+++ PokemonProject/src/Pokemon/HasieraPantalla.java
@@ -0,0 +1,43 @@
+package Pokemon;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+
+public class HasieraPantalla extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HasieraPantalla frame = new HasieraPantalla();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HasieraPantalla() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 610, 399);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+	}
+
+}
